89c36fedd96cd782f70ebf2f475c633768576f86
bhchen-0914 <364293032@qq.com>
2021-03-22 00:11:23 +0800

    新增Chapter4 ：HttpClient框架代码

---
+++ Chapter4/src/main/java/com/course/httpClient/cookies/MyCookiesForPost.java
@@ -0,0 +1,64 @@
+package com.course.httpClient.cookies;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MyCookiesForPost {
+
+    private ResourceBundle bundle; //用于读取配置文件
+    private CookieStore store;
+    HttpGet get;
+    String url;
+    String uri;
+
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA); //配置显示中文
+        url = bundle.getString("test.url");
+        uri = bundle.getString("test.post.withCookies");
+        get = new HttpGet(this.url+this.uri);
+    }
+
+    /**
+     * 获取cookies信息
+     */
+    @Test
+    public void testGetCookies() throws IOException {
+        //实例化一个HttpClientContext对象，用于保留请求信息
+        HttpClientContext context = HttpClientContext.create();
+        CloseableHttpClient client = HttpClients.createDefault();
+        //传入context实例，原本作用为：多个请求序列传入相同context实例，可保证请求之间的信息状态的自动传播
+        //这里用于获取本次的cookies信息
+        client.execute(get,context);
+        this.store = context.getCookieStore();
+        //返回一个元素为Cookie对象的List列表
+        List<Cookie> cookieList = store.getCookies();
+        //使用增强for循环遍历cookies信息
+        for (Cookie cookie:cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name:"+name + " cookie value:"+value);
+        }
+
+    }
+
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testPostWithCookies(){
+        String testUrl = this.url + this.bundle.getString("test.post.withCookies");
+        HttpPost post = new HttpPost(testUrl);
+
+    }
+
+}
